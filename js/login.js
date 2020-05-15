26710824c565e8d10dfcc2c2c0431816a7124443
TheLoGgI <lasse_aakjaer@hotmail.com>
2020-05-16 00:22:00 +0200

    login system med localstorage

---
+++ js/login.js
@@ -0,0 +1,64 @@
+
+const registreform = document.getElementById('registreform')
+const registresubmit = registreform.querySelector('input[type="submit"]')
+
+
+registresubmit.addEventListener('click', function(e) {
+    e.preventDefault()
+    let errorMsg = document.getElementById('error')
+    errorMsg.style.color = 'red'
+    // Create user login, with localStorage
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    const repassword = document.getElementById('repassword').value
+
+    if (!email) {
+        errorMsg.textContent = 'udfyld venligst email felt'
+    } else if(!password) {
+        errorMsg.textContent = 'udfyld venligst kodeord felt'
+    } else if (!repassword) {      
+        errorMsg.textContent = 'Gentag venligst kodeord felt'
+    }  else {
+        errorMsg.textContent = ''
+    }
+
+    if (password && repassword) {
+        if (password !== repassword) {
+            errorMsg.textContent = 'Kodeord stemte ikke overens'
+       } else {
+           errorMsg.textContent = ''
+        //    Everything is good
+           localStorage.setItem('email', email)
+           localStorage.setItem('password', password)
+           errorMsg.style.color = 'darkgreen'
+           errorMsg.textContent = 'Du er nu registreret'
+       }
+    }
+}) 
+
+const loginform = document.getElementById('loginform')
+const loginsubmit = loginform.querySelector('input[type="submit"]')
+loginsubmit.addEventListener('click', function(e) {
+    e.preventDefault()
+    let errorMsg = document.getElementById('loginerror')
+    errorMsg.style.color = 'red'
+    // Create user login, with localStorage
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    alert('Du er logget ind')
+})
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
