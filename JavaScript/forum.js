e642c89329a85533e3b7f2578219ba6003613298
madshoe47 <56107464+madshoe47@users.noreply.github.com>
2020-05-25 10:55:05 +0200

    funktion

--- JavaScript/forum.js
+++ JavaScript/forum.js
@@ -1,4 +1,5 @@
 // Lytter til begge navigationer
+document.getElementById('udfold').addEventListener('click', enableDropdownMenu)
 document.getElementById('pilhvid').addEventListener('click', enableDropdownMenu)
 
 // Synlig og usynliggøre menuen med de 5 seneste indlæg
@@ -7,11 +8,14 @@ let menuEnabelt = false
 function enableDropdownMenu(e) {
     e.preventDefault()
     const menu = document.getElementById('wrapwrapwrap')
+    const pil = document.getElementById('pilhvid')
 
     if (menuEnabelt) {
         menu.style.display = 'block'
 
+
     } else {
         menu.style.display = 'none'
+        pil.style.transform = 'rotate(0deg)'
     }
     menuEnabelt = !menuEnabelt
